82a8c473a639fb42de4aa333d4d75084f463d409
lrks <yuke1222@gmail.com>
2015-10-21 23:46:44 +0900

    まーこれで良いでしょう？

--- utzo/utzo.ino
+++ utzo/utzo.ino
@@ -3,24 +3,19 @@ const int SP_BACK = 11;
 const int SW_PORT = 8;
 const int LED_PORT = 7;
 
-const int VOLUME = 128;
 const int FD = 200;
 const int BD = FD;
 const int ISI = -50;
 const int BACK_START = FD + ISI;
 const int TOTAL_END = BACK_START + BD;
+const float SIN_COEF = 0.00157079633; // (2pi) / ((1 / f[Hz])[us])
 
 bool running_flg;
 unsigned long start_time;
 
 void setup() {
   running_flg = false;
-  //TCCR2B = ((TCCR2B & B11111000) | B00000101);
-  
-  TCCR2B = TCCR2B & B11111000 | B00000101;
-  //TCCR2B = TCCR2B & B11111000 | B00000110;
-  //TCCR2B = TCCR2B & B11111000 | B00000111;
-  
+  TCCR2B = TCCR2B & B11111000 | B00000001;  // 32KHz
   pinMode(SP_FRONT, OUTPUT);
   pinMode(SP_BACK, OUTPUT);
   pinMode(SW_PORT, INPUT_PULLUP);
@@ -34,7 +29,7 @@ void startDevice()
   if (running_flg) return;
 
   running_flg = true;
-  start_time = millis();
+  start_time = micros();
 }
 
 void recvSerial()
@@ -52,14 +47,28 @@ void checkButton()
   startDevice();
 }
 
-void driveDevice(unsigned long nowtime)
+void driveDevice(unsigned long us)
 {
-  bool flg = (nowtime < TOTAL_END);
+  unsigned long ms = us / 1000;
+  bool flg = (ms < TOTAL_END);
+
+  // Front
+  if (ms < FD) {
+    int v = (int)(127.5 * (sin(SIN_COEF * us) + 1.0));
+    analogWrite(SP_FRONT, v);
+  } else {
+    analogWrite(SP_FRONT, 0);
+  }
+
+  // Back
+  if ((BACK_START < ms) && flg) {
+    int v = (int)(127.5 * (sin(SIN_COEF * (us - (BACK_START * 1000))) + 1.0));
+    analogWrite(SP_BACK, v);
+  } else {
+    analogWrite(SP_BACK, 0);
+  }
   
-  analogWrite(SP_FRONT, (nowtime < FD) ? VOLUME : 0);
-  analogWrite(SP_BACK, ((BACK_START < nowtime) && flg) ? VOLUME : 0);
   digitalWrite(LED_PORT, flg ? HIGH : LOW);
-  
   if (flg) return;
   running_flg = false;
 }
@@ -68,6 +77,6 @@ void loop() {
   recvSerial();
   checkButton();
   if (!running_flg) return;
-  driveDevice(millis() - start_time);
+  driveDevice(micros() - start_time);
 }
 
